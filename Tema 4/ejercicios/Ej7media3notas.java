c60dbcf4a4c74bf6c6d7152737f13137f10975f1
moises <moises_rodiguez@hotmail.com>
2015-10-08 09:15:07 +0200

    Ejercio 7

--- Tema 4/ejercicios/Ej7media3notas.java
+++ Tema 4/ejercicios/Ej7media3notas.java
@@ -1,27 +1,30 @@
 /**
- * Realiza un programa que √
- *calcule el tiempo que tardará en caer un objeto desde una altura
- * h. Aplica la fórmula t =2h g siendo g = 9.81m/s 2
+ * Realiza un programa que calcule la media de tres notas.
  *
  * @author Moisés Rodríguez Naranjo
  */
 
-public class Ej6tiempotardaencaer {
+public class Ej7media3notas {
   public static void main(String[] args) {
-    System.out.print("Dime la altura ");
+    System.out.print("Introduce la nota 1: ");
     String linea = System.console().readLine();
-    double altura = Integer.parseInt( linea );
+    double nota1 = Double.parseDouble( linea );
     
-    double g;
-    g = 9.81;
+    System.out.print("Introduce la nota 2: ");
+    String linea2 = System.console().readLine();
+    double nota2 = Double.parseDouble( linea2 );
+    
+    System.out.print("Introduce la nota 3: ");
+    String linea3 = System.console().readLine();
+    double nota3 = Double.parseDouble( linea3 );
     
     double total1;
-    total1 = 2 * altura / g;
-    
     double total2;
-    total2 = Math.sqrt(total1);
+    total1 = nota1 + nota2 + nota3;
+    total2 = total1 / 3;
     
-    System.out.print("El tiempo que tardará es " + total2);
+    
+    System.out.printf("%.2f", total2);
     
           
   }
