f33276a9ebf7d22539d800a597abb6edb6c2e74b
moises <moises_rodiguez@hotmail.com>
2015-10-22 13:41:52 +0200

    Subido Ejercicio 13 Tema 5

--- Tema 5/Ej13LeeNumerosYDiceNegativoOPositivo.java
+++ Tema 5/Ej13LeeNumerosYDiceNegativoOPositivo.java
@@ -1,14 +1,23 @@
 /**
  * Escribe un programa que lea una lista de diez números y determine cuántos son positivos,
- *y cuántos son negativos.
+ * y cuántos son negativos.
  */
 public class Ej13LeeNumerosYDiceNegativoOPositivo{
   
   public static void main(String[] args) {
-  
-  for (int i =0; i < 11; i++){
+    int positivo = 0;
+    int negativo = 0;
     
-  }
-   
+     System.out.println("Introduce 10 números");
+  
+    for (int i =0; i < 10; i++){
+      if (Integer.parseInt(System.console().readLine()) < 0) {
+        negativo ++;
+      } else {
+        positivo++;
+      }
+     
+    }
+    System.out.println("Has introducido " + positivo + " números positivos " + " y " + negativo + " números negativos");
   }
 }
