cb3ddf2780fce2687ff308b983e4cfcba3124d43
moises <moises_rodiguez@hotmail.com>
2015-11-02 18:48:32 +0100

    Ejercicios subidos Tema 6

---
+++ Tema 6/Ej1TresTiradasDado.java
@@ -0,0 +1,23 @@
+/**
+ * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar también la suma
+ * total (los puntos que suman entre los tres dados). El programa pregunta si hacer otras 3
+ * tiradas.
+ *
+ * @author Moisés Rodríguez Naranjo
+ */
+public class Ej1TresTiradasDado {
+  public static void main(String[] args) {
+    
+    int tirada = 0;
+    int suma = 0;
+   
+ 
+     
+    for (int i = 1; i <= 3; i++) {
+      tirada = (int)(Math.random()*6 + 1);
+      System.out.println("Tirada" + i + " --> " + tirada + " ");
+      suma += tirada;  
+    }
+    System.out.println("La suma de las 3 tiradas es: " + suma); 
+  }
+}
