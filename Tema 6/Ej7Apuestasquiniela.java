fea7794b43691793a620a6fa706a24d6e3ef752a
moises <moises_rodiguez@hotmail.com>
2015-11-03 11:13:08 +0100

    Subido ejercicios Tema 6

---
+++ Tema 6/Ej7Apuestasquiniela.java
@@ -0,0 +1,12 @@
+/**
+ * Escribe un programa que piense un número al azar entre 0 y 100. El usuario debe adivinarlo
+ * y tiene para ello 5 oportunidades. Después de cada intento fallido, el programa dirá cuántas
+ * oportunidades quedan y si el número introducido es menor o mayor que el número secreto.
+ *
+ * @author Moisés Rodríguez Naranjo
+ */
+public class Ej7Apuestasquiniela {
+  public static void main(String[] args) {
+    
+  }
+}
