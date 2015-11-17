640606b246d4bb89f95509175b5a6721778194e7
moises <moises_rodiguez@hotmail.com>
2015-11-17 10:14:23 +0100

    Subido ejercicios  Tema 7

---
+++ Tema 7/Ej3ArrayLee10NumerosYMuestraEnOrdenInverso.java
@@ -0,0 +1,25 @@
+
+/**
+ * Describe un programa que lea 10 números por teclado y que luego los muestre en orden
+ * inverso, es decir, el primero que se introduce es el último en mostrarse y viceversa.
+ * 
+ * @author Moisés Rodríguez Naranjo
+ */
+
+public class Ej3ArrayLee10NumerosYMuestraEnOrdenInverso {
+  public static void main(String[] args) {
+        
+    int[] n = new int[10]; //define n como array de enteros
+    //Lee 10 números y los guarda en el array
+    System.out.println("Introdice 10 numeros: ");
+    for (int i = 0 ; i < 10; i++){ 
+      n[i] = Integer.parseInt(System.console().readLine());
+    }
+    System.out.print("Numeros mostrados en orden inverso: ");
+    //Muestra los números guardados en orden inverso
+    for (int i = 9 ; i > -1; i--){ //contador descendente
+      System.out.print(n[i] + " ");
+    }
+    
+  }
+}
