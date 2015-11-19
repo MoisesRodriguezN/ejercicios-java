424233d9d507118b2568b0a008ee9c41366c54e0
moises <moises_rodiguez@hotmail.com>
2015-11-19 10:26:02 +0100

    Correcciones Ejercicio 1

--- Tema 7/Ej1ArrayDe12Mostrar.java
+++ Tema 7/Ej1ArrayDe12Mostrar.java
@@ -14,13 +14,12 @@ public class Ej1ArrayDe12Mostrar {
             
     num[0] = 39;
     num[1] = -2;
-    num[4] = 0 ;
     num[6] = 14;
     num[8] = 5 ;
     num[9] = 120;
     
-    System.out.print("Los valores del array n son los siguientes: ");
-    for (int i = 0; i < 12; i++){ //EL bucle recorre el array y lo muestra
+    System.out.print("Los valores del array son los siguientes: ");
+    for (int i = 0; i < 12; i++){ //El bucle recorre el array y lo muestra
       System.out.print(num[i] + " ");
     }
     
