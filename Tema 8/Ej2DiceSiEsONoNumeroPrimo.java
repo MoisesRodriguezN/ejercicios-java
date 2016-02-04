ad2a34ef92bd06e5ede89860bdc7b97260d901e2
moises <moises_rodiguez@hotmail.com>
2016-02-04 10:22:30 +0100

    Ejercicio 2 Funciones

---
+++ Tema 8/Ej2DiceSiEsONoNumeroPrimo.java
@@ -0,0 +1,29 @@
+/**
+ * Escribe un programa que diga si un número introducido por teclado es o no primo. Un
+ * número primo es aquel que sólo es divisible entre él mismo y la unidad.
+ * @author Moisés Rodríguez
+ */
+public class Ej2DiceSiEsONoNumeroPrimo{
+  
+   public static void main(String[] args) {
+           
+    System.out.print("Introduce un número para comprobar si es primo: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
+    
+    boolean esPrimo = true; 
+    
+    for (int i = 2; i < numeroIntroducido && esPrimo; i++) { 
+      if ((numeroIntroducido % i)  == 0 ) { // ¿es divisible por i?
+        esPrimo = false; 
+      }
+    }
+        
+    if (esPrimo) {
+      System.out.println("El número introducido es primo.");
+    } else {
+      System.out.println("El número introducido no es primo.");
+    }
+
+    
+  }
+}
